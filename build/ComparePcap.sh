c4f1b022d647054cb586d9258fa3c528ed96574a
Kevin Denis <kdenis@arkoon.net>
2013-04-30 11:35:32 +0200

    Correct right values with right messages

--- build/ComparePcap.sh
+++ build/ComparePcap.sh
@@ -6,8 +6,8 @@ $TSHARK -r $2 -V | gawk -f $BUILD_DIR/ComparePcap.awk - > $(basename $2).txt
 $DIFF $(basename $1).txt $(basename $2).txt > $(basename $1).diff
 RET=$?
 
-echo "original pcap file is $PWD/$1"
-echo "generated pcap file is $2"
+echo "original pcap file is $2"
+echo "generated pcap file is $PWD/$1"
 echo "diff file is $PWD/$(basename $1).diff"
 
 cat $(basename $1).diff
