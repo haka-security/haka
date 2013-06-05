88e72c77015e0d5e218e8e181d642e0b24dadeb5
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-06-05 16:16:08 +0200

    Fix shellcode rule

--- tests/sample/shellcode-attack.lua
+++ tests/sample/shellcode-attack.lua
@@ -1,13 +1,13 @@
 require("ipv4")
 require("tcp")
 
-local bindshell =	"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b" ..
-					"\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd" ..
-					"\x80\xe8\xdc\xff\xff\xff/bin/sh"
+local bindshell = "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b" ..
+                  "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd" ..
+                  "\x80\xe8\xdc\xff\xff\xff/bin/sh"
 
 haka.rule {
-    hooks = { "tcp-up" },
-    eval = function (self, pkt)
+	hooks = { "tcp-up" },
+	eval = function (self, pkt)
 		if #pkt.payload > 0 then
 			-- reconstruct payload
 			payload = ''
@@ -17,9 +17,9 @@ haka.rule {
 			-- test if shellcode is present in data
 			if string.find(payload, bindshell) then
 				haka.log("filter", "/bin/sh shellcode detected !!!")
-				tcp:drop()
-        	end
-		end 
-	end	
+				pkt:drop()
+			end
+		end
+	end
 }
 
