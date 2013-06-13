1baf6e14f71947cacafe36d62787977a149ee652
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-06-13 18:41:45 +0200

    Fix test log level
    The log level for the test should be DEBUG.

--- build/TestPcapCompare.lua
+++ build/TestPcapCompare.lua
@@ -2,4 +2,6 @@ local arg = {...}
 
 haka.app.install("packet", haka.module.load("packet-pcap", "-f", arg[2], "-o", arg[3]))
 
+haka.log.setlevel(haka.log.DEBUG)
+
 haka.app.load_configuration(arg[1])
--- build/TestPcapOutput.lua
+++ build/TestPcapOutput.lua
@@ -2,4 +2,6 @@ local arg = {...}
 
 haka.app.install("packet", haka.module.load("packet-pcap", "-f", arg[2]))
 
+haka.log.setlevel(haka.log.DEBUG)
+
 haka.app.load_configuration(arg[1])
