027ee54ab0de2faa17de8efc01330315796689e8
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-06-18 11:59:03 +0200

    Fix security rule configuration sample

--- tests/sample/security-rules.lua
+++ tests/sample/security-rules.lua
@@ -12,17 +12,25 @@ require("http")
 -- Function definition
 ------------------------------------
 
-function getpayload(data)
+local function getpayload(data)
 	payload = ''
 	for i = 1, #data do
-		payload = payload .. string.format("%c", data[i])
+		payload = payload .. string.char(data[i])
 	end
 	return payload
 end
 
 
-function contains(table, elem)
+local function contains(table, elem)
 	return table[elem] ~= nil
+end
+
+local function dict(table)
+	local ret = {}
+	for _, v in pairs(table) do
+		ret[v] = true
+	end
+	return ret
 end
 
 
@@ -35,7 +43,7 @@ haka.rule {
 	eval = function (self, pkt)
 		-- bad IP checksum
 		if not pkt:verify_checksum() then
-			haka.log("filter", "\tBad IP checksum")
+			haka.log.error("filter", "Bad IP checksum")
 			pkt:drop()
 		end
 	end
@@ -49,7 +57,7 @@ haka.rule {
 	hooks = { "ipv4-up" },
 	eval = function (self, pkt)
 		if pkt.src == pkt.dst then
-			haka.log("filter", "Land attack detected")
+			haka.log.error("filter", "Land attack detected")
 			pkt:drop()
 		end
 	end
@@ -64,7 +72,7 @@ haka.rule {
 	eval = function (self, pkt)
 		--Xmas scan, as made by nmap -sX <IP>
 		if pkt.flags.psh and pkt.flags.fin and pkt.flags.urg then
-			haka.log("filter", "Xmas attack detected !!!")
+			haka.log.error("filter", "Xmas attack detected !!!")
 			pkt:drop()
 		end
 	end
@@ -81,7 +89,7 @@ haka.rule {
 			payload = getpayload(pkt.payload)
 			-- test if shellcode is present in data
 			if string.find(payload, bindshell) then
-				haka.log("filter", "/bin/sh shellcode detected !!!")
+				haka.log.error("filter", "/bin/sh shellcode detected !!!")
 				pkt:drop()
 			end
 		end
@@ -95,10 +103,10 @@ haka.rule {
 haka.rule {
 	hooks = {"http-request"},
 	eval = function (self, http)
-		local http_methods = { 'get', 'post', 'head', 'put', 'trace', 'delete', 'options' }
-		local method = http.request.method
-		if contains(http_methods, method:lower()) == nil then
-			haka.log("filter", "non authorized http method")
+		local http_methods = dict({ 'get', 'post', 'head', 'put', 'trace', 'delete', 'options' })
+		local method = http.request.method:lower()
+		if not contains(http_methods, method) then
+			haka.log.error("filter", "non authorized http method '%s'", method)
 			http:drop()
 		end
 	end
@@ -108,11 +116,11 @@ haka.rule {
 haka.rule {
 	hooks = {"http-request"},
 	eval = function (self, http)
-		local http_versions = { '0.9', '1.0', '1.1' }
+		local http_versions = dict({ '0.9', '1.0', '1.1' })
 		local protocol = http.request.version:sub(1,4)
 		local version = http.request.version:sub(6)
-		if not protocol == "HTTP" or contains(http_versions, version) == nil then
-			haka.log("filter", "unsupported http version")
+		if not protocol == "HTTP" or not contains(http_versions, version) then
+			haka.log.error("filter", "unsupported http version '%s/%s'", protocol, version)
 			http:drop()
 		end
 	end
@@ -124,8 +132,9 @@ haka.rule {
 	eval = function (self, http)
 		local content_length = http.request.headers["Content-Length"]
 		if content_length then
-			if tonumber(content_length) or content_length < 0 then
-				haka.log("filter", "corrupted content-length header value")
+			content_length = tonumber(content_length)
+			if content_length == nil or content_length < 0 then
+				haka.log.error("filter", "corrupted content-length header value")
 				http:drop()
 			end
 		end
@@ -138,9 +147,9 @@ haka.rule {
 
 -- add custom user-agent
 haka.rule {
-	hooks = {"http-response"},
+	hooks = {"http-request"},
 	eval = function (self, http)
-		http.response.headers["User-Agent"] = "Haka User-Agent"
+		http.request.headers["User-Agent"] = "Haka User-Agent"
 	end
 }
 
@@ -149,9 +158,8 @@ haka.rule {
 	hooks = {"http-request"},
 	eval = function (self, http)
 		local method = http.request.method:lower()
-		print(method)
-		if not method == 'get' and not method == 'post' then
-			haka.log("filter", "not allowed http method")
+		if method ~= 'get' and method ~= 'post' then
+			haka.log.warning("filter", "forbidden http method '%s'", method)
 		end
 	end
 }
@@ -170,17 +178,16 @@ haka.rule {
 			nikto	= '.+%(Nikto%/.+%)%s%(Evasions:.+%)%s%(Test:.+%)',
 			nessus	= '^Nessus.*',
 			w3af	= '*.%s;w3af.sf.net%)',
-			sqlmap	= '^sqlmap%/%.*%s(http:%/%/sqlmap%.sourceforge%.net)',
+			sqlmap	= '^sqlmap%/.*%s%(http:%/%/sqlmap.*%)',
 			fimap	= '^fimap%.googlecode%.%com.*',
 			grabber	= '^Grabber.*'
 		}
 
 		if (http.request.headers['User-Agent']) then
 			local user_agent = http.request.headers['User-Agent']
-			print('[' .. user_agent .. ']')
 			for scanner, pattern in pairs(http_useragent) do
 				if user_agent:match(pattern) then
-					haka.log("filter", "Nikto scan detected !!!")
+					haka.log.error("filter", "'%s' scan detected !!!", scanner)
 					http:drop()
 				end
 			end
@@ -195,14 +202,13 @@ haka.rule {
 akpf = haka.rule_group {
 	name = "akpf",
 	init = function (self, pkt)
-		haka.log("filter", "entering packet filetring rules : %d --> %d", pkt.tcp.srcport, pkt.tcp.dstport)
+		haka.log.debug("filter", "entering packet filetring rules : %d --> %d", pkt.tcp.srcport, pkt.tcp.dstport)
 	end,
 	fini = function (self, pkt)
-		haka.log("filter", "packet dropped : drop by default")
+		haka.log.error("filter", "packet dropped : drop by default")
 		pkt:drop()
 	end,
 	continue = function (self, ret)
-		print ("ret:" .. tostring(ret))
 		return not ret
 	end
 }
@@ -215,13 +221,14 @@ akpf:rule {
 		local netsrc = ipv4.network("10.2.96.0/22")
 		local netdst = ipv4.network("10.2.104.0/22")
 
---		if netsrc:contains(pkt.ip.src) and
---			netdst:contains(pkt.ip.dst) and
---			pkt.dstport == 80 then
-		if pkt.tcp.dstport == 8888 or pkt.tcp.dstport == 8008 then
-			haka.log("filter", "authorizing http traffic")
-			pkt.next_dissector = "http"
+		local tcp = pkt.tcp
 
+		if netsrc:contains(tcp.ip.src) and
+		   netdst:contains(tcp.ip.dst) and
+		   tcp.dstport == 80 then
+
+			haka.log.warning("filter", "authorizing http traffic")
+			pkt.next_dissector = "http"
 			return true
 		end
 	end
@@ -237,10 +244,10 @@ akpf:rule {
 		local tcp = pkt.tcp
 
 		if netsrc:contains(tcp.ip.src) and
-			netdst:contains(tcp.ip.dst) and
-			tcp.dstport == 22 then
+		   netdst:contains(tcp.ip.dst) and
+		   tcp.dstport == 22 then
 
-			haka.log("filter", "authorizing ssh traffic")
+			haka.log.warning("filter", "authorizing ssh traffic")
 			haka.log.warning("filter", "no available dissector for ssh")
 			return true
 		end
