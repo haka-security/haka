9e21b9f17d0a9ccd51c262a4ff38c132ef1a18b2
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-06-18 12:16:14 +0200

    Add configuration sample files
    The configuration files are also installed to 'share/haka'.

--- src/conf/capture-nfq.lua
+++ src/conf/capture-nfq.lua
@@ -26,13 +26,10 @@ local function split(str, delim)
 end
 
 local ifaces = split(arg[1], ":")
-haka.app.install("packet", haka.module.load("packet-nfqueue", "-p", "input.pcap", "output.pcap", "drop.pcap", unpack(ifaces)))
+haka.app.install("packet", haka.module.load("packet-nfqueue", unpack(ifaces)))
 
 haka.app.install("log", haka.module.load("log-stdout"))
 
 haka.app.load_configuration(arg[2])
-
-haka.log.setlevel(haka.log.DEBUG)
-haka.log.setlevel("packet", haka.log.INFO)
 
 haka.log.info("config", "done\n")
--- src/conf/capture-pcap.lua
+++ src/conf/capture-pcap.lua
@@ -12,7 +12,4 @@ haka.app.install("log", haka.module.load("log-stdout"))
 
 haka.app.load_configuration(arg[2])
 
-haka.log.setlevel(haka.log.DEBUG)
-haka.log.setlevel("packet", haka.log.INFO)
-
 haka.log.info("config", "done\n")
