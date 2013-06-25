097cc43514fc1ede75cdc86a170613b302f8dcd1
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-06-25 15:56:11 +0200

    Fix path splitting for absolute path
    The absolute path was not split correctly and generated a relative path.

--- src/lua/utils.lua
+++ src/lua/utils.lua
@@ -1,14 +1,13 @@
 -- functions used by different modules
+
 function split(str, delim)
 	local ret = {}
 	local last_end = 1
 	local s, e = str:find(delim, 1)
 
 	while s do
-		if s ~= 1 then
-			cap = str:sub(last_end, e-1)
-			table.insert(ret, cap)
-		end
+		cap = str:sub(last_end, e-1)
+		table.insert(ret, cap)
 
 		last_end = e+1
 		s, e = str:find(delim, last_end)
@@ -23,12 +22,14 @@ function split(str, delim)
 end
 
 function get_file_directory(str)
-	splitpath=split(str,"/")
+	local splitpath = split(str, "/")
 	local newpath = ""
+
 	if #splitpath > 1 then
 		for i = 1,#splitpath-1 do
 			newpath = newpath .. splitpath[i] .. "/"
 		end
 	end
+
 	return newpath
 end
