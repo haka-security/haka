85718ca39f555fb007ee4bcba31586ba64c5684d
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-06-28 11:47:32 +0200

    Add versioning for Haka

---
+++ include/haka/macro.h
@@ -0,0 +1,9 @@
+
+#ifndef _HAKA_MACRO_H
+#define _HAKA_MACRO_H
+
+#define _STR(v)		#v
+
+#define STR(v)		_STR(v)
+
+#endif /* _HAKA_MACRO_H */
---
+++ include/haka/version.h
@@ -0,0 +1,13 @@
+
+#ifndef _HAKA_VERSION_H
+#define _HAKA_VERSION_H
+
+#include <haka/macro.h>
+#include <haka/config.h>
+
+
+#define HAKA_API_VERSION    1
+
+#define HAKA_VERSION        HAKA_VERSION_MAJOR "." HAKA_VERSION_MINOR "." HAKA_VERSION_BUILD
+
+#endif /* _HAKA_VERSION_H */
