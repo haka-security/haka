baff522b75d83115f7197ef920a33c52b84ebf88
Kevin Denis <kdenis@arkoon.net>
2013-07-01 11:07:21 +0200

    Don't detect land attack for localhost

--- sample/standard/proto/ipv4/security.lua
+++ sample/standard/proto/ipv4/security.lua
@@ -5,7 +5,7 @@
 haka.rule {
 	hooks = { "ipv4-up" },
 	eval = function (self, pkt)
-		if pkt.src == pkt.dst then
+		if (pkt.src == pkt.dst) and (pkt.src ~= ipv4.addr("127.0.0.1")) then
 			haka.log.error("filter", "Land attack detected")
 			pkt:drop()
 		end
