be4da1998bc28c0e93c0469aa357099c8866631e
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-07-08 16:17:41 +0200

    Remove wrong doc
    The documentation is in sphinx now.

--- modules/ipv4/main.c
+++ modules/ipv4/main.c
@@ -11,34 +11,6 @@ static void cleanup()
 {
 }
 
-/**
- * @defgroup IPv4 IPv4
- * @brief IPv4 dissector API fucntions and structures.
- * @author Arkoon Network Security
- * @ingroup ExternProtocolModule
- *
- * # Description
- *
- * The module dissects IPv4 Header
- *
- * The modules allows to access and modify all IP header field values
- *
- * # Usage
- *
- * ### Lua
- * ~~~~~~~~{.lua}
- * ipv4 = require("proto-ipv4")
- * app.install_filter(function (pkt)
- * ip = ipv4(pkt)
- * --[ get ip header field value (print)
- * log.debug("filter", "version: %d", ip.version)
- * --[ set ip header field value
- * ip.ttl = 20
- * return packet.ACCEPT
- * end)
- * ~~~~~~~~
- */
-
 struct module HAKA_MODULE = {
 	type:        MODULE_EXTENSION,
 	name:        L"IPv4",
