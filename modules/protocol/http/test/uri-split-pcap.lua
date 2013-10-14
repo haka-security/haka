444d6e23383dac1092dba0805a907956b0ad2577
Kevin Denis <kdenis@arkoon.net>
2013-10-14 11:45:27 +0200

    Add test to verify that splitting is done once

---
+++ modules/protocol/http/test/uri-split-pcap.lua
@@ -0,0 +1,41 @@
+require("protocol/ipv4")
+require("protocol/tcp")
+require("protocol/http")
+
+haka.rule {
+	hooks = { "tcp-connection-new" },
+	eval = function (self, pkt)
+		if pkt.tcp.dstport == 80 then
+			pkt.next_dissector = "http"
+		end
+	end
+}
+
+haka.rule {
+	hooks = { 'http-request' },
+	eval = function (self, pkt)
+		local uri = pkt.request.uri
+		print('--> splitting uri')
+		splitted = pkt.request:split_uri()
+		print(splitted)
+	end
+}
+
+haka.rule {
+	hooks = { 'http-request' },
+	eval = function (self, pkt)
+		local uri = pkt.request.uri
+		print('--> splitting uri')
+		splitted2 = pkt.request:split_uri()
+		print(splitted2)
+		--Check that tables are the same
+		--Same in the way that this is the same memory, not only the same content 
+		if splitted == splitted2
+		then
+			print("Table are the same")
+		else
+			print("Table are different")
+		end
+	end
+}
+
