31ac823c5d8160e4435be7596b43e7eb9e001e50
Kevin Denis <kdenis@arkoon.net>
2013-10-23 15:46:53 +0200

    Create tcpfilter.lua configuration file and pcap file

---
+++ sample/tutorial/filter/tcpfilter.lua
@@ -0,0 +1,29 @@
+--The require lines tolds Haka which dissector
+--it has to register. Those dissectors gives hook
+--to interfere with it.
+require("protocol/ipv4")
+require("protocol/tcp")
+
+--This is a rule
+haka.rule {
+        --The hooks tells where this rule is applied
+        --Only TCP packet will be concerned by this rule
+        --Other protocol will flow
+        hooks = {"tcp-up"},
+        --Each rule have an eval function
+        --Eval function is always build with (self, name)
+                --First parameter, self, is mandatory
+                --Second parameter can be named whatever you want
+        eval = function (self, pkt)
+                --All fields is accessible through accessors
+                --following wireshark/tcpdump semantics
+                --Documentation give the complet list of accessors
+				--You can choose to let the packet flow, or block it.
+				if pkt.dstport ~= 31337 then
+					-- We want to block this connexion
+					pkt:drop()
+					haka.log.info("Filter","This is not port 31337")
+				end
+				--All other packets will be accepted
+        end
+}
