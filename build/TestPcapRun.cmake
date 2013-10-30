eb7ac4cae9c4b68154cbc49b63bd7c4ea2721cd2
Kevin Denis <kdenis@arkoon.net>
2013-10-30 16:21:00 +0100

    Really fix test
    The LC_ALL variable modify the formatting of the output of
    a test. Forcing to C fix the results to the good known format.

--- build/TestPcapRun.cmake
+++ build/TestPcapRun.cmake
@@ -1,5 +1,6 @@
 
 set(ENV{LANG} "C")
+set(ENV{LC_ALL} "C")
 set(ENV{BUILD_DIR} ${CTEST_MODULE_DIR})
 set(ENV{DIFF} ${DIFF})
 set(ENV{TSHARK} ${TSHARK})
@@ -10,7 +11,7 @@ set(ENV{LD_LIBRARY_PATH} ${TEST_RUNDIR}/${HAKA_INSTALL_PREFIX}/lib)
 set(CMAKE_MODULE_PATH ${CTEST_MODULE_DIR} ${CMAKE_MODULE_PATH})
 include(Valgrind)
 
-message("Executing LANG=\"C\" LUA_PATH=\"$ENV{LUA_PATH}\" HAKA_PATH=\"$ENV{HAKA_PATH}\" LD_LIBRARY_PATH=\"$ENV{LD_LIBRARY_PATH}\" ${EXE} -d --no-pass-through ${EXE_OPTIONS} -o ${DST}.pcap ${SRC} ${CONF}")
+message("Executing LANG=\"C\" LC_ALL=\"C\" LUA_PATH=\"$ENV{LUA_PATH}\" HAKA_PATH=\"$ENV{HAKA_PATH}\" LD_LIBRARY_PATH=\"$ENV{LD_LIBRARY_PATH}\" ${EXE} -d --no-pass-through ${EXE_OPTIONS} -o ${DST}.pcap ${SRC} ${CONF}")
 
 VALGRIND(${DST} ${EXE} -d --no-pass-through ${EXE_OPTIONS} -o ${DST}.pcap ${SRC} ${CONF}
 	RESULT_VARIABLE HAD_ERROR OUTPUT_FILE ${DST}-tmp.txt)
