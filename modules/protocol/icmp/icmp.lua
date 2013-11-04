359b08b3af2dfb83e85749dcba4b30ad53942fe8
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-11-04 11:50:32 +0100

    Rename icmp internals
    Rename internal icmp data to hide them from the user. Also renamed the
    ip packet to be able to access it doing 'icmp.ip'.

--- modules/protocol/icmp/icmp.lua
+++ modules/protocol/icmp/icmp.lua
@@ -13,52 +13,52 @@ function icmp_dissector.receive(pkt)
 end
 
 function icmp_dissector.method:__init(pkt)
-	self.pkt = pkt
-	self.payload = pkt.payload
+	self.ip = pkt
+	self._payload = pkt.payload
 end
 
 icmp_dissector.property.type = {
 	get = function (self)
-		return self.payload:sub(0, 1):asnumber('big')
+		return self._payload:sub(0, 1):asnumber('big')
 	end,
 	set = function (self, num)
-		return self.payload:sub(0, 1):setnumber(num, 'big')
+		return self._payload:sub(0, 1):setnumber(num, 'big')
 	end
 }
 
 icmp_dissector.property.code = {
 	get = function (self)
-		return self.payload:sub(1, 1):asnumber('big')
+		return self._payload:sub(1, 1):asnumber('big')
 	end,
 	set = function (self, num)
-		return self.payload:sub(1, 1):setnumber(num, 'big')
+		return self._payload:sub(1, 1):setnumber(num, 'big')
 	end
 }
 
 icmp_dissector.property.checksum = {
 	get = function (self)
-		return self.payload:sub(2, 2):asnumber('big')
+		return self._payload:sub(2, 2):asnumber('big')
 	end,
 	set = function (self, num)
-		return self.payload:sub(2, 2):setnumber(num, 'big')
+		return self._payload:sub(2, 2):setnumber(num, 'big')
 	end
 }
 
 function icmp_dissector.method:verify_checksum()
-	return ipv4.inet_checksum(self.pkt.payload) == 0
+	return ipv4.inet_checksum(self._payload) == 0
 end
 
 function icmp_dissector.method:compute_checksum()
 	self.checksum = 0
-	self.checksum = ipv4.inet_checksum(self.pkt.payload)
+	self.checksum = ipv4.inet_checksum(self._payload)
 end
 
 function icmp_dissector.method:continue()
-	return self.pkt:continue()
+	return self.ip:continue()
 end
 
 function icmp_dissector.method:drop()
-	return self.pkt:drop()
+	return self.ip:drop()
 end
 
 function icmp_dissector.method:emit()
@@ -74,7 +74,7 @@ function icmp_dissector.method:emit()
 end
 
 local function icmp_dissector_build(icmp)
-	if icmp.payload.modified then
+	if icmp._payload.modified then
 		icmp:compute_checksum()
 	end
 end
@@ -89,12 +89,12 @@ function icmp_dissector.method:send()
 	end
 
 	icmp_dissector_build(self)
-	self.pkt:send()
+	self.ip:send()
 end
 
 function icmp_dissector.method:inject()
 	icmp_dissector_build(self)
-	self.pkt:inject()
+	self.ip:inject()
 end
 
 function icmp_dissector.create(pkt)
