02ac1e2a10342b6a5df40a4a8f9b8d5586294567
Kevin Denis <kdenis@arkoon.net>
2013-11-05 11:08:42 +0100

    Modify test name for tcpfilter to groupfilter

--- sample/tutorial/filter/groupfilter.lua
+++ sample/tutorial/filter/groupfilter.lua
@@ -9,23 +9,6 @@ require('protocol/tcp')
 -- Security group rule
 ------------------------------------
 
-haka.rule {
-	-- The hooks tells where this rule is applied
-	-- Only TCP packets will be concerned by this rule
-	-- Other protocol will flow
-	hooks = { 'tcp-up' },
-		eval = function (self, pkt)
-			-- We want to accept connection to or from
-			-- TCP port 80 (request/response)
-			if pkt.dstport == 80 or pkt.srcport == 80 then
-				haka.log.info("Filter", "Authorizing trafic on port 80")
-			else
-				pkt:drop()
-				haka.log.info("Filter", "This is not TCP port 80")
-			end
-        end
-}
-
 local group = haka.rule_group {
 	-- The hooks tells where this rule is applied
 	-- only TCP packets will be concerned by this rule
@@ -43,13 +26,12 @@ local group = haka.rule_group {
 	end
 }
 
-
 group:rule {
-	hooks = {"tcp-up"},
+	hooks = { 'tcp-connection-new' },
 	eval = function (self, pkt)
 		-- We want to accept connection to or from
 		-- TCP port 80 (request/response)
-		if pkt.dstport == 80 or pkt.srcport == 80 then
+		if pkt.dstport == 80 then
 			haka.log("Filter", "Authorizing trafic on port 80")
 			return true
 		end
@@ -57,13 +39,12 @@ group:rule {
 }
 
 group:rule {
-	hooks = {"tcp-up"},
+	hooks = { 'tcp-connection-new' },
 	eval = function (self, pkt)
-		--local tcp = pkt.tcp
 		-- We want to accept connection to or from
 		-- TCP port 22 (request/response)
-		if pkt.dstport == 80 or pkt.srcport == 80 then
-			haka.log("Filter", "Authorizing trafic on port 80")
+		if pkt.dstport == 22 then
+			haka.log("Filter", "Authorizing trafic on port 22")
 			return true
 		end
 	end
