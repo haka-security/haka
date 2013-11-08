b71c41d36420abbdca5c96f13d8340fdb1a5394c
Kevin Denis <kdenis@arkoon.net>
2013-11-08 16:22:58 +0100

    Add an haka.alert in filter tutorial

--- sample/tutorial/filter/ipfilter.lua
+++ sample/tutorial/filter/ipfilter.lua
@@ -18,8 +18,11 @@ haka.rule {
 		local bad_ip = ipv4.addr('192.168.10.10')
 		if pkt.src == bad_ip then
 			-- We want to block this IP
+			haka.alert{
+				description = string.format("Filtering IP %s", bad_ip),
+				severity = 'low'
+			}
 			pkt:drop()
-			haka.log.info("Filter", "Filtering IP %s", bad_ip)
 		end
 		-- All other packets will be accepted
 	end
