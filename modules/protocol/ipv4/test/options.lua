19105ff688d22b38af4884ab9fd14497a3f2d8ea
Pierre-Sylvain Desse <psdesse@arkoon.net>
2013-12-04 15:48:06 +0100

    Add missing MPLv2 license header

--- modules/protocol/ipv4/test/options.lua
+++ modules/protocol/ipv4/test/options.lua
@@ -1,3 +1,7 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 -- Basic test that will output ip options (if present)
 
 require("protocol/ipv4")
--- src/test/state_machine.lua
+++ src/test/state_machine.lua
@@ -1,6 +1,8 @@
---[[
-This test check the state machine transitions.
-]]
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+-- This test check the state machine transitions.
 
 require('protocol/ipv4')
 
