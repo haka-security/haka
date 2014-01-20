f1d78d4d7eb4d41ff122564d3bbf0d2cae857fc8
Pierre-Sylvain Desse <psdesse@arkoon.net>
2014-01-20 18:10:12 +0100

    Cleanup tests
    Fix broken tests after Git history cleanup.

--- modules/protocol/ipv4/test/nat.lua
+++ modules/protocol/ipv4/test/nat.lua
@@ -7,8 +7,8 @@ local ipv4 = require('protocol/ipv4')
 haka.rule {
 	hooks = { "ipv4-up" },
 	eval = function (self, pkt)
-		if pkt.dst == ipv4.addr("10.2.253.137") then
-			pkt.dst = ipv4.addr("192.168.1.137")
+		if pkt.dst == ipv4.addr("192.168.10.1") then
+			pkt.dst = ipv4.addr("192.168.110.1")
 		end
 	end
 }
