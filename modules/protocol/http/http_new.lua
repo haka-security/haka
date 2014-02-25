1d447dfe720b0d3aa6e0852f0a67627fe2a9c893
Mehdi Talbi <mtalbi@arkoon.net>
2014-02-25 18:18:11 +0100

    Specify HTTP using grammar and state machine API
    This is a partial specification of http protocol

--- modules/protocol/http/http_new.lua
+++ modules/protocol/http/http_new.lua
@@ -1,188 +1,380 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+require("protocol/tcp-connection")
 
 local module = {}
 
-local http = haka.dissector.new('http', { 'http-request', 'http-response' })
+local str = string.char
 
-static(http).getdata = function (cls, stream)
-	local data = stream.connection.data._http
-	if not data then
-		data = cls:new(stream)
-		stream.connection.data._http = data
+
+--
+-- HTTP utilities
+--
+
+local function contains(table, elem)
+	return table[elem] ~= nil
+end
+
+local function dict(table)
+	local ret = {}
+	for _, v in pairs(table) do
+		ret[v] = true
 	end
-
-	data._tcp_stream = stream
-	return data
+	return ret
 end
 
-function http:__init(stream)
-	self.__super.__init(self)
-	self.connection = stream.connection
-	self._state = 0
-end
+local _unreserved = dict({45, 46, 95, 126})
 
-function http:valid()
-	return self._tcp_stream:valid()
-end
-
-function http:drop()
-	return self._tcp_stream:drop()
-end
-
-function http:reset()
-	return self._tcp_stream:reset()
-end
-
-function http:forge()
-	local tcp = self._tcp_stream
-	if tcp then
-		if self._state == 2 and tcp.direction then
-			self._state = 3
-
-			if haka.packet.mode() ~= haka.packet.PASSTHROUGH then
-				tcp.stream:seek(self.request._mark, true)
-				self.request._mark = nil
-
-				tcp.stream:erase(self.request._length)
-				tcp.stream:insert(self.request.method)
-				tcp.stream:insert(" ")
-				tcp.stream:insert(self.request.uri)
-				tcp.stream:insert(" ")
-				tcp.stream:insert(self.request.version)
-				tcp.stream:insert("\r\n")
-				build_headers(tcp.stream, self.request.headers, self.request._headers_order)
-				tcp.stream:insert("\r\n")
+local function uri_safe_decode(uri)
+	local uri = string.gsub(uri, '%%(%x%x)',
+		function(p)
+			local val = tonumber(p, 16)
+			if (val > 47 and val < 58) or
+			   (val > 64 and val < 91) or
+			   (val > 96 and val < 123) or
+			   (contains(_unreserved, val)) then
+				return str(val)
+			else
+				return '%' .. string.upper(p)
 			end
+		end)
+	return uri
+end
 
-		elseif self._state == 5 and not tcp.direction then
-			self._state = 0
-
-			if haka.packet.mode() ~= haka.packet.PASSTHROUGH then
-				tcp.stream:seek(self.response._mark, true)
-				self.response._mark = nil
-
-				tcp.stream:erase(self.response._length)
-				tcp.stream:insert(self.response.version)
-				tcp.stream:insert(" ")
-				tcp.stream:insert(self.response.status)
-				tcp.stream:insert(" ")
-				tcp.stream:insert(self.response.reason)
-				tcp.stream:insert("\r\n")
-				build_headers(tcp.stream, self.response.headers, self.response._headers_order)
-				tcp.stream:insert("\r\n")
-			end
+local function uri_safe_decode_split(tab)
+	for k, v in pairs(tab) do
+		if type(v) == 'table' then
+			uri_safe_decode_split(v)
+		else
+			tab[k] = uri_safe_decode(v)
 		end
-
-		self._tcp_stream = nil
 	end
-	return tcp
 end
 
-function http:dissect()
-	if self._tcp_stream.direction then
-		if self._state == 0 or self._state == 1 then
-			if self._tcp_stream.stream:available() > 0 then
-				if self._state == 0 then
-					self.request = {}
-					self.response = nil
-					self._state = 1
+local _prefixes = {{'^%.%./', ''}, {'^%./', ''}, {'^/%.%./', '/'}, {'^/%.%.$', '/'}, {'^/%./', '/'}, {'^/%.$', '/'}}
+
+local function remove_dot_segments(path)
+	local output = {}
+	local slash = ''
+	local nb = 0
+	if path:sub(1,1) == '/' then slash = '/' end
+	while path ~= '' do
+		local index = 0
+		for _, prefix in ipairs(_prefixes) do
+			path, nb = path:gsub(prefix[1], prefix[2])
+			if nb > 0 then
+				if index == 2 or index == 3 then
+					table.remove(output, #output)
 				end
-
-				parse(self, self.request, parse_request, "request", 2)
+				break
 			end
-		elseif self.request then
-			self.next_dissector = self.request.next_dissector
+			index = index + 1
 		end
-	else
-		if self._state == 3 or self._state == 4 then
-			if self._tcp_stream.stream:available() > 0 then
-				if self._state == 3 then
-					self.response = {}
-					self._state = 4
-				end
-
-				parse(self, self.response, parse_response, "response", 5)
-			end
-		elseif self.response then
-			self.next_dissector = self.response.next_dissector
+		if nb == 0 then
+			if path:sub(1,1) == '/' then path = path:sub(2) end
+			local left, right = path:match('([^/]*)([/]?.*)')
+			table.insert(output, left)
+			path = right
 		end
 	end
+	return slash .. table.concat(output, '/')
+end
+
+-- register methods on splitted uri object
+local mt_uri = {}
+mt_uri.__index = mt_uri
+
+function mt_uri:__tostring()
+	local uri = {}
+
+	-- authority components
+	local auth = {}
+
+	-- host
+	if self.host then
+		-- userinfo
+		if self.user and self.pass then
+			table.insert(auth, self.user)
+			table.insert(auth, ':')
+			table.insert(auth, self.pass)
+			table.insert(auth, '@')
+		end
+
+		table.insert(auth, self.host)
+
+		--port
+		if self.port then
+			table.insert(auth, ':')
+			table.insert(auth, self.port)
+		end
+	end
+
+	-- scheme and authority
+	if #auth > 0 then
+		if self.scheme then
+			table.insert(uri, self.scheme)
+			table.insert(uri, '://')
+			table.insert(uri, table.concat(auth))
+		else
+			table.insert(uri, table.concat(auth))
+		end
+	end
+
+	-- path
+	if self.path then
+		table.insert(uri, self.path)
+	end
+
+	-- query
+	if self.query then
+		local query = {}
+		for k, v in pairs(self.args) do
+			local q = {}
+			table.insert(q, k)
+			table.insert(q, v)
+			table.insert(query, table.concat(q, '='))
+		end
+
+		if #query > 0 then
+			table.insert(uri, '?')
+			table.insert(uri, table.concat(query, '&'))
+		end
+	end
+
+	-- fragment
+	if self.fragment then
+		table.insert(uri, '#')
+		table.insert(uri, self.fragment)
+	end
+
+	return table.concat(uri)
 end
 
 
-local states = http.states
+function mt_uri:normalize()
+	assert(self)
+	-- decode percent-encoded octets of unresserved chars
+	-- capitalize letters in escape sequences
+	uri_safe_decode_split(self)
 
-states.request = states:state {
-	[haka.state_machine.on_input("request")] = function (http, request)
-		http.request = request
-		return "reponse"
-	end,
-	[haka.state_machine.on_timeout(100)] = function (http)
-		return "ERROR"
+	-- use http as default scheme
+	if not self.scheme and self.authority then
+		self.scheme = 'http'
+	end
+
+	-- scheme and host are not case sensitive
+	if self.scheme then self.scheme = string.lower(self.scheme) end
+	if self.host then self.host = string.lower(self.host) end
+
+	-- remove default port
+	if self.port and self.port == '80' then
+		self.port = nil
+	end
+
+	-- add '/' to path
+	if self.scheme == 'http' and (not self.path or self.path == '') then
+		self.path = '/'
+	end
+
+	-- normalize path according to rfc 3986
+	if self.path then self.path = remove_dot_segments(self.path) end
+
+	return self
+
+end
+
+
+local function uri_split(uri)
+	if not uri then return nil end
+
+	local splitted_uri = {}
+	local core_uri
+	local query, fragment, path, authority
+
+	setmetatable(splitted_uri, mt_uri)
+
+	-- uri = core_uri [ ?query ] [ #fragment ]
+	core_uri, query, fragment =
+	    string.match(uri, '([^#?]*)[%?]*([^#]*)[#]*(.*)')
+
+	-- query (+ split params)
+	if query and query ~= '' then
+		splitted_uri.query = query
+		local args = {}
+		string.gsub(splitted_uri.query, '([^=&]+)=([^&?]*)&?',
+		    function(p, q) args[p] = q return '' end)
+		splitted_uri.args = args
+	end
+
+	-- fragment
+	if fragment and fragment ~= '' then
+		splitted_uri.fragment = fragment
+	end
+
+	-- scheme
+	local temp = string.gsub(core_uri, '^(%a*)://',
+	    function(p) if p ~= '' then splitted_uri.scheme = p end return '' end)
+
+	-- authority and path
+	authority, path = string.match(temp, '([^/]*)([/]*.*)$')
+
+	if (path and path ~= '') then
+		splitted_uri.path = path
+	end
+
+	-- authority = [ userinfo @ ] host [ : port ]
+	if authority and authority ~= '' then
+		splitted_uri.authority = authority
+		-- userinfo
+		authority = string.gsub(authority, "^([^@]*)@",
+		    function(p) if p ~= '' then splitted_uri.userinfo = p end return '' end)
+		-- port
+		authority = string.gsub(authority, ":([^:][%d]+)$",
+		    function(p) if p ~= '' then splitted_uri.port = p end return '' end)
+		-- host
+		if authority ~= '' then splitted_uri.host = authority end
+		-- userinfo = user : password (deprecated usage)
+		if not splitted_uri.userinfo then return splitted_uri end
+
+		local user, pass = string.match(splitted_uri.userinfo, '(.*):(.*)')
+		if user and user ~= '' then
+			splitted_uri.user = user
+			splitted_uri.pass = pass
+		end
+	end
+	return splitted_uri
+end
+
+local function uri_normalize(uri)
+	local splitted_uri = uri_split(uri)
+	splitted_uri:normalize()
+	return tostring(splitted_uri)
+end
+
+
+-- register methods on splitted cookie list
+local mt_cookie = {}
+mt_cookie.__index = mt_cookie
+
+function mt_cookie:__tostring()
+	assert(self)
+	local cookie = {}
+	for k, v in pairs(self) do
+		local ck = {}
+		table.insert(ck, k)
+		table.insert(ck, v)
+		table.insert(cookie, table.concat(ck, '='))
+	end
+	return table.concat(cookie, ';')
+end
+
+local function cookies_split(cookie_line)
+	local cookies = {}
+	if cookie_line then
+		string.gsub(cookie_line, '([^=;]+)=([^;?]*);?',
+		    function(p, q) cookies[p] = q return '' end)
+	end
+	setmetatable(cookies, mt_cookie)
+	return cookies
+end
+
+module.uri = {}
+module.cookies = {}
+module.uri.split = uri_split
+module.uri.normalize = uri_normalize
+module.cookies.split = cookies_split
+
+
+--
+-- HTTP dissector
+--
+
+local http_dissector = haka.dissector.new{
+	type = haka.dissector.FlowDissector,
+	name = 'http'
+}
+
+http_dissector:register_event('request')
+http_dissector:register_event('response')
+
+--
+-- HTTP State Machine
+--
+
+http_dissector.states = http.state_machine.new("http")
+
+http_dissector.states.request = http_dissector.states:state{
+	input = function (context, http)
+		context.input = 'request'
+		return context.states.response
 	end
 }
 
-states.response = states:state {
-	[haka.state_machine.on_input("response")] = function (http, response)
-		http.response = response
-		return "request"
-	end,
-	[haka.state_machine.on_timeout(100)] = function (http)
-		return "ERROR"
+http_dissector.states.request = http_dissector.states:state{
+	input = function (context, http)
+		context.input = 'response'
+		return context.states.request
 	end
 }
 
-states.initial = "request"
+http_dissector.states.initial = http_dissector.states.request
 
+--
+-- HTTP Grammar
+--
 
-http.grammar = haka.grammar.new()
-local grammar = http.grammar
+-- http separator tokens
+local WS = haka.grammar.token('[[:blank:]]+')
+local CRLF = haka.grammar.token('[%r]?%n')
 
-grammar.WS = haka.grammar.re('[ \t]+')
-grammar.CRLF = haka.grammar.re('[\r]?\n')
-grammar.TOKEN = haka.grammar.re('[^()<>@,;:\\\"\\/\\[\\]?={} \t]+')
-grammar.URI = haka.grammar.re('[A-Za-z\\.]+')
-
-grammar.version = haka.grammar.record{
-	haka.grammar.re('HTTP/'),
-	haka.grammar.re('[0-9]+\\.[0-9]+'):as('_num'),
+-- http request version
+local version = haka.grammar.record{
+	haka.grammar.token('HTTP/'),
+	haka.grammar.field('_num', haka.grammar.token('[0-9]+%.[0-9]+'))
 }:extra{
 	num = function (self)
 		return toint(self._num)
 	end
 }
 
-grammar.status = haka.grammar.record{
-	haka.grammar.re('[0-9]{3}'):as('_num'),
+-- http response status code
+local status = haka.grammar.record{
+	haka.grammar.token('_num', haka.grammar.token('[0-9]{3}'))
 }:extra{
 	num = function (self)
 		return tonumber(self._num)
 	end
 }
 
+-- http request line
 grammar.request_line = haka.grammar.record{
-	grammar.TOKEN:as('method'),
-	grammar.WS,
-	grammar.URI:as('uri'),
-	grammar.WS,
-	grammar.version:as('version'),
-	grammar.CRLF
+	haka.grammar.field('method', haka.grammar.token('[ˆ()<>@,;:\\"/%[%]?={}[:blank:]]+')),
+	WS,
+	haka.grammar.field('uri', haka.grammar.token('[[:alnum][:punct]]+')),
+	WS,
+	haka.grammar.field('version', version),
+	CRLF
 }
 
+-- http reply line
 grammar.reply_line = haka.grammar.record{
-	grammar.version:as('version'),
-	grammar.WS,
-	grammar.status:as('status'),
-	grammar.WS,
-	haka.grammar.re('[^\r\n]+'):as('reason'),
-	grammar.CRLF
+	haka.grammar.field('version', version),
+	WS,
+	grammar.grammar.field('status', status),
+	WS,
+	haka.grammar.field('reason', haka.grammar.token('[^%r%n]+')),
+	CRLF
 }
 
-grammar.header = haka.grammar.record{
-	haka.grammar.re('[^: \t]+'):as('name'),
-	haka.grammar.re(':'),
-	grammar.WS,
-	haka.grammar.re('[^\r\n]+'):as('value'),
-	grammar.CRLF
+-- http header
+local header = haka.grammar.record{
+	haka.grammar.field('name', haka.grammar.token('[^:[:blank:]]+')),
+	haka.grammar.token(':'),
+	WS,
+	haka.grammar.field('value', haka.grammar.token('[^%r%n]+')),
+	CRLF
 }:extra{
 	function (self, context)
 		local hdr_name = string.lower(self.name)
@@ -196,14 +388,16 @@ grammar.header = haka.grammar.record{
 	end
 }
 
-grammar.headers = haka.grammar.record{
-	haka.grammar.array(grammar.header),
-	grammar.CRLF
+-- http headers list
+local headers = haka.grammar.record{
+	haka.grammar.array(header),
+	CRLF
 }
 
-grammar.chunk = haka.grammar.record{
-	haka.grammar.re('[0-9a-fA-F]+'):as('_chunk_size'),
-	grammar.CRLF,
+-- http chunk
+local chunk = haka.grammar.record{
+	haka.grammar.field('_chunk_size', haka.grammar.token('[0-9a-fA-F]+')),
+	CRLF,
 	haka.grammar.bytes():options{
 		count = function (self, context) return self.count end
 	}
@@ -211,39 +405,81 @@ grammar.chunk = haka.grammar.record{
 	count = function (self) return tonumber(self._chunk_size, 16) end
 }
 
-grammar.chunks = haka.grammar.record{
-	haka.grammar.array(grammar.chunk):options{
+-- http chunks list
+local chunks = haka.grammar.record{
+	haka.grammar.array(chunk):options{
 		untilcond = function (elem) return elem.count == 0 end
 	},
-	grammar.headers
+	headers
 }
 
-grammar.body = haka.grammar.record{
+local body = haka.grammar.record{
 	haka.grammar.branch(
 		{
 			content = haka.grammar.bytes():options{
 				'chunked',
 				count = function (self, context) return context.content_length end
 			},
-			chunked = grammar.chunks
+			chunked = chunks
 		}, 
 		function (context) return context.mode end
 	)
 }
 
-grammar.message = haka.grammar.record{
-	grammar.headers:as('headers'),
-	grammar.body:as('body')
+local message = haka.grammar.record{
+	haka.grammar.field('headers', headers),
+	haka.grammar.field('body', body)
 }
 
-grammar.request = haka.grammar.record{
-	grammar.request_line:as('request'),
-	grammar.message:as('msg')
+-- http request
+local request = haka.grammar.record{
+	haka.grammar.field('request', request_line),
+	haka.grammar.field('msg', message),
 }
 
-grammar.reply = haka.grammar.record{
-	grammar.reply_line:as('reply'),
-	grammar.message:as('msg')
+-- http response
+local reply = haka.grammar.record{
+	haka.grammar.field('response', response_line)
+	haka.grammar.field('msg', message),
 }
+
+
+http_dissector.grammar = haka.grammar.branch(
+	{
+		request = request,
+		response = response
+	},
+	function (context) return context.input end	
+):compile()
+
+http_dissector.property.connection = { 
+	get = function (self)
+		self.connection = self.flow.connection
+		return self.connection
+	end 
+}
+
+function http_dissector.method:__init(flow)
+	super(http_dissector).__init(self)
+	self.flow = flow
+	if flow then
+		self.connection = flow.connection
+	end 
+	self._state = 0 
+end
+
+function http_dissector.method:continue()
+	return self.flow ~= nil 
+end
+
+function http_dissector.method:drop()
+	self.flow:drop()
+	self.flow = nil 
+end
+
+function http_dissector.method:reset()
+	self.flow:reset()
+	self.flow = nil 
+end
 
 return module
