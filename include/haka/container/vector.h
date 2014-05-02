ae2c1aab2590edc806659d892e419ec5e4023e02
Pierre-Sylvain Desse <psdesse@arkoon.net>
2014-05-02 15:19:12 +0200

    Fix vector static initializer

--- include/haka/container/vector.h
+++ include/haka/container/vector.h
@@ -18,7 +18,7 @@ struct vector {
 	void         (*destruct)(void *elem);
 };
 
-#define VECTOR_INIT(type, destruct)       {0, sizeof(type), 0, NULL, destruct}
+#define VECTOR_INIT(type, destruct)       {sizeof(type), 0, 0, NULL, destruct}
 
 #define vector_create(v, type, destruct)                  _vector_create((v), sizeof(type), 0, (destruct))
 #define vector_create_reserve(v, type, count, destruct)   _vector_create((v), sizeof(type), (count), (destruct))
