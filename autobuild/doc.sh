b1aaef7d2a818bee86afe9383616549fb4dff6cc
Pierre-Sylvain Desse <psdesse@arkoon.net>
2014-05-13 18:04:59 +0200

    Add doc copy in autobuild script

--- autobuild/doc.sh
+++ autobuild/doc.sh
@@ -6,6 +6,12 @@ source autobuild/includes.sh
 _doc() {
 	cd "$MAKEDIR/$1-$2"
 	_run make doc
+
+	if [ -n "$DROPDIR" ]; then
+		### Copy to destination
+		_run mkdir -p "$DROPDIR/"
+		_run cp -r doc/doc "$DROPDIR/"
+	fi
 }
 
 _runeach _doc
