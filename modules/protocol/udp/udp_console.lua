7ad261372298b1a1a73c9ed6a5248b897b1933b3
Pierre-Sylvain Desse <psdesse@arkoon.net>
2014-06-10 16:51:20 +0200

    Add udp console utilities

---
+++ modules/protocol/udp/udp_console.lua
@@ -0,0 +1,44 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+local list = require('list')
+
+require('protocol/ipv4')
+
+local UdpCnxInfo = list.new('UdpCnxInfo')
+
+UdpCnxInfo.field = {
+	'id', 'srcip', 'srcport', 'dstip', 'dstport', 'state',
+	'in_pkts', 'in_bytes', 'out_pkts', 'out_bytes'
+}
+
+UdpCnxInfo.key = 'id'
+
+UdpCnxInfo.field_format = {
+	['in_pkt']    = list.formatter.unit,
+	['in_bytes']  = list.formatter.unit,
+	['out_pkt']   = list.formatter.unit,
+	['out_bytes'] = list.formatter.unit
+}
+
+function UdpCnxInfo.method:drop()
+	for _,r in ipairs(self._data) do
+		local id = r.id
+		hakactl.remote(r.thread, function ()
+			haka.console.udp.drop_connection(id)
+		end)
+	end
+end
+
+console.udp = {}
+
+function console.udp.connexions(show_dropped)
+	local data = hakactl.remote('all', function ()
+		return haka.console.udp.list_connections(show_dropped)
+	end)
+
+	local conn = UdpCnxInfo:new()
+	conn:addall(data)
+	return conn
+end
