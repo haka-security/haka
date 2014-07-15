746020f56e9d213143ab063eabb34e940b518a76
Pierre-Sylvain Desse <psdesse@arkoon.net>
2014-07-15 14:58:05 +0200

    Add ipv4 fragmentation test

---
+++ modules/protocol/ipv4/test/fragment.lua
@@ -0,0 +1,20 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+-- Test ipv4 reassembly
+
+local raw = require("protocol/raw")
+local ipv4 = require("protocol/ipv4")
+local icmp = require("protocol/icmp")
+
+haka.rule {
+	hook = icmp.events.receive_packet,
+	eval = function (pkt)
+		haka.log("test", "received icmp packet size=%d", #pkt.payload)
+		
+		if #pkt.payload > 2000 then
+			pkt.payload:pos(1476):insert(haka.vbuffer_from("HAKAHAKA"))
+		end
+	end
+}
