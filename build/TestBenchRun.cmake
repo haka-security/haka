e5bbbbaf56b0842de4c33f53475c6d780e6451fa
Paul Fariello <pfariello@arkoon.net>
2014-09-22 18:38:03 +0200

    Use hakabench and haka from pretests install
    Allow to copy/paste executed command to test again without having to source
    out/env.sh

--- build/TestBenchRun.cmake
+++ build/TestBenchRun.cmake
@@ -6,6 +6,7 @@ set(ENV{LANG} "C")
 set(ENV{LC_ALL} "C")
 set(ENV{BUILD_DIR} ${CTEST_MODULE_DIR})
 set(ENV{LUA_PATH} ${PROJECT_SOURCE_DIR}/src/lua/?.lua)
+set(ENV{HAKA} ${HAKA})
 set(ENV{HAKA_PATH} ${HAKA_PATH})
 set(ENV{PATH} $ENV{PATH}:${HAKA_PATH}/sbin:${HAKA_PATH}/bin)
 set(ENV{LD_LIBRARY_PATH} ${HAKA_PATH}/lib:${HAKA_PATH}/lib/haka/modules/protocol:${HAKA_PATH}/lib/haka/modules/packet)
@@ -13,7 +14,7 @@ set(ENV{TZ} Europe/Paris)
 
 set(CMAKE_MODULE_PATH ${CTEST_MODULE_DIR} ${CMAKE_MODULE_PATH})
 
-message("Executing TZ=\"Europe/Paris\" LANG=\"C\" LC_ALL=\"C\" LUA_PATH=\"$ENV{LUA_PATH}\" HAKA_PATH=\"$ENV{HAKA_PATH}\" LD_LIBRARY_PATH=\"$ENV{LD_LIBRARY_PATH}\" ${EXE} ${EXE_OPTIONS} ${BENCH}")
+message("Executing TZ=\"Europe/Paris\" LANG=\"C\" LC_ALL=\"C\" LUA_PATH=\"$ENV{LUA_PATH}\" HAKA=\"$ENV{HAKA}\" HAKA_PATH=\"$ENV{HAKA_PATH}\" LD_LIBRARY_PATH=\"$ENV{LD_LIBRARY_PATH}\" ${EXE} ${EXE_OPTIONS} ${BENCH}")
 
 execute_process(COMMAND ${EXE} ${EXE_OPTIONS} ${BENCH} RESULT_VARIABLE HAD_ERROR)
 
