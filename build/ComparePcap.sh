30ab2a0c27b4ff579d210c9fab8d0939ea0f10f5
Pierre-Sylvain Desse <psdesse@arkoon.net>
2014-10-09 17:40:48 +0200

    Improve pcap compare
    If the comparison failed, this error was not caught during the tests.

--- build/ComparePcap.sh
+++ build/ComparePcap.sh
@@ -6,11 +6,19 @@
 TSHARK_ARGS="-o ip.check_checksum:TRUE -o udp.check_checksum:TRUE -o tcp.check_checksum:TRUE"
 
 $TSHARK $TSHARK_ARGS -Vr $1 | gawk -f $BUILD_DIR/ComparePcap.awk - > $(basename $1).txt
+if [ $? != 0 ]; then
+	echo "Pcap compare failed"
+	exit 1
+fi
+
 $TSHARK $TSHARK_ARGS -Vr $2 | gawk -f $BUILD_DIR/ComparePcap.awk - > $(basename $2).txt
+if [ $? != 0 ]; then
+	echo "Pcap compare failed"
+	exit 1
+fi
 
 $DIFF -u $(basename $1).txt $(basename $2).txt > $(basename $1).diff
 RET=$?
 
 cat $(basename $1).diff
 exit $RET
-
