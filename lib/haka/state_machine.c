ca3bb40feecb6db793494e40b5240666e198a2d7
Pierre-Sylvain Desse <psdesse@arkoon.net>
2014-10-13 09:17:17 +0200

    Fix state machine Lua ref issue
    If the weak ref has been release, the state machine should not call the
    Lua.

--- lib/haka/state_machine.c
+++ lib/haka/state_machine.c
@@ -518,6 +518,10 @@ void state_machine_instance_finish(struct state_machine_instance *instance)
 
 void state_machine_instance_destroy(struct state_machine_instance *instance)
 {
+	if (!instance->finished) {
+		state_machine_instance_finish(instance);
+	}
+
 	if (instance->context) {
 		instance->context->destroy(instance->context);
 	}
