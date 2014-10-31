a6f1be31db5fa70509a924c7611c00bcca2432a3
Paul Fariello <pfariello@arkoon.net>
2014-10-31 15:52:31 +0100

    Move include to usefull file

--- build/UseLua.cmake
+++ build/UseLua.cmake
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+include(UseBc2c)
+
 if(CMAKE_LUA STREQUAL "luajit")
 	include(external/luajit/luajit.cmake)
 elseif(CMAKE_LUA STREQUAL "lua")
