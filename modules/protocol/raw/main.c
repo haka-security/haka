dd6be67a2b4e402df6c838f2959cc7e4b90b977b
Paul Fariello <pfariello@arkoon.net>
2014-10-31 15:57:24 +0100

    Merge raw.c into main.c

--- modules/protocol/raw/main.c
+++ modules/protocol/raw/main.c
@@ -2,8 +2,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <lua.h>
+#include <lauxlib.h>
+
+#include <haka/config.h>
+#include <haka/luabinding.h>
 #include <haka/module.h>
 
+#include "lua/raw.h"
+
+#ifdef HAKA_FFI
+LUA_BIND_INIT(raw)
+{
+	LUA_LOAD(raw, L);
+	lua_call(L, 0, 1);
+
+	return 1;
+}
+#endif
 
 static int init(struct parameters *args)
 {
