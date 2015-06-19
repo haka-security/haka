5e31dd90186a431a813bd1c3c8882b7f3d1a320e
Pierre-Sylvain Desse <psdesse@arkoon.net>
2015-06-19 14:24:45 +0200

    Fix tests
    Fix workshop code test and add a new one for the new blurring attempt
    code.

--- doc/user/workshop/blurring-the-web.lua
+++ doc/user/workshop/blurring-the-web.lua
@@ -7,7 +7,7 @@ local rem = require('regexp/pcre')
 
 http.install_tcp_rule(80)
 
-local pattern = '</head>'
+local pattern = '<head>'
 local regexp = rem.re:compile(pattern, rem.re.CASE_INSENSITIVE)
 local css = ' <style type="text/css" media="screen"> * { color: transparent !important; text-shadow: 0 0 3px black !important; } </style> '
 
@@ -37,8 +37,7 @@ haka.rule{
 		streamed = true,
 	},
 	eval = function (flow, iter)
-		local result = regexp:match(iter)
-		if result then
+		if regexp:match(iter) then
 			haka.log("bluring response")
 			iter:insert(haka.vbuffer_from(css))
 		end
